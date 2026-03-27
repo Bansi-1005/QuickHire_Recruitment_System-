0ba7431cb77f788dfdb64cba5c240df97e4c1eb8
Bansi-1005 <tejanibansi05@gmail.com>
2026-03-27 12:39:00 +0530

    commit changes Bansi created AdminResource and RecruiterResource

---
+++ Quick_Hire/src/main/java/com/mycompany/quick_hire/resources/RecruiterResource.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quick_hire.resources;
+
+import jakarta.ws.rs.Path;
+
+/**
+ *
+ * @author tejan
+ */
+
+@Path("recruiter")
+public class RecruiterResource {
+    
+}
